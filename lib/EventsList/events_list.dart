e5861f6cdaea6a8f7dd73da638a36ced317eae6d
Terry9110 <terelouti9110@gmail.com>
2022-02-16 15:43:37 -0800

    added changes

--- lib/EventsList/events_list.dart
+++ lib/EventsList/events_list.dart
@@ -1,3 +1,4 @@
+import 'package:campus2/EventsDetail/events_details_widget.dart';
 import 'package:campus2/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -5,178 +6,213 @@ import 'package:provider/provider.dart';
 import 'package:campus2/EventsDetail/model/event_list_model.dart';
 
 class EventsList extends StatefulWidget {
+  const EventsList({Key? key}) : super(key: key);
   @override
   State<EventsList> createState() => _EventsList();
 }
 
 class _EventsList extends State<EventsList> {
-  // var _eventsList = null;
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    // _eventsList = Provider.of<EventListModel>(context, listen: false);
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      final provider = Provider.of<EventListModel>(context, listen: false);
+      provider.getEventsList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var _eventsList = Provider.of<EventListModel>(context, listen: false);
+    final provider = Provider.of<EventListModel>(context);
+    return SafeArea(
+        top: true,
+        child: Scaffold(
+            bottomNavigationBar: bottomBar(),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                topBar(),
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.only(
+                      left: 20, right: 20, bottom: 10, top: 0),
+                  child: ListView(
+                    children: [
+                      for (Map<String, dynamic> event in provider.eventsList)
+                        EventDetails(
+                          imageUrl: event["image_url"],
+                          eventName: event["event_name"],
+                          subtitle: event["subtitle"],
+                          date: event["event_date"],
+                          place: event["place"],
+                          totalInterested: event["total_interested"],
+                          totalViews: event["total_views"],
+                          organizerName: event["organizer_name"],
+                        ),
+                      Visibility(
+                          visible: provider.isLoading,
+                          child: Column(children: const [
+                            SizedBox(height: 30),
+                            CircularProgressIndicator()
+                          ])),
+                      Visibility(
+                          visible: provider.errMessage != null,
+                          child: Column(children: [
+                            const SizedBox(height: 30),
+                            Text(provider.errMessage ?? "",
+                                style: GoogleFonts.poppins(
+                                  color: Colors.red,
+                                ))
+                          ]))
+                    ],
+                  ),
+                ))
+              ],
+            )));
+  }
 
-    return Scaffold(
-      bottomNavigationBar: Container(
-          height: 70,
-          padding: const EdgeInsets.only(left: 0, right: 0),
-          decoration: BoxDecoration(
-            color: activeGreenPrimary.withOpacity(0.8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey[500]!,
-                blurRadius: 10.0,
-              ),
-            ],
+  Widget bottomBar() {
+    return Container(
+        height: 70,
+        padding: const EdgeInsets.only(left: 0, right: 0),
+        decoration: BoxDecoration(
+          color: activeGreenPrimary.withOpacity(0.8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[500]!,
+              blurRadius: 10.0,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                Text('Home',
+                    style:
+                        GoogleFonts.poppins(color: Colors.white, fontSize: 12))
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                    onTap: () {
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => const AddNewEvent()));
+                    },
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white70,
+                    )),
+                Text('Add New Event',
+                    style: GoogleFonts.poppins(
+                        color: Colors.white54, fontSize: 12))
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.list_outlined,
+                  color: Colors.white70,
+                ),
+                Text('Tab 3',
+                    style: GoogleFonts.poppins(
+                        color: Colors.white54, fontSize: 12))
+              ],
+            ),
+          ],
+        ));
+  }
+
+  Widget topBar() {
+    double width = MediaQuery.of(context).size.width;
+    return Stack(children: [
+      Container(
+          width: width,
+          height: 100,
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            color: activeGreenPrimary,
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              const Icon(Icons.menu, color: Colors.white),
+              const SizedBox(width: 20),
+              Text('Events',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600)),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.white,
-                  ),
-                  Text('Tab1',
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 12))
+                children: const [
+                  Icon(Icons.notifications_outlined, color: Colors.white),
+                  SizedBox(width: 5),
+                  Icon(Icons.near_me_outlined, color: Colors.white),
+                  SizedBox(width: 5),
+                  Icon(Icons.filter_alt_outlined, color: Colors.white)
                 ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.white,
-                  ),
-                  Text('Tab2',
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 12))
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.white,
-                  ),
-                  Text('Tab3',
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 12))
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.white,
-                  ),
-                  Text('Tab4',
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 12))
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.circle,
-                    color: Colors.white,
-                  ),
-                  Text('Tab5',
-                      style: GoogleFonts.poppins(
-                          color: Colors.white, fontSize: 12))
-                ],
-              ),
+              ))
             ],
           )),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(15),
-            decoration: const BoxDecoration(
-              color: activeGreenPrimary,
-            ),
-            child: Center(
-                child: Text("Events",
-                    style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.bold, color: Colors.white))),
-          ),
-          Container(
-              decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[500]!,
-                  blurRadius: 10.0,
-                ),
-              ]),
-              padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(children: [
-                    Text("Events",
-                        style: GoogleFonts.poppins(
-                            color: Colors.black, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 10),
-                    Container(
-                      height: 5,
-                      width: 60,
-                      color: Colors.green,
-                    )
-                  ]),
-                  Text("Calendar",
-                      style: GoogleFonts.poppins(color: Colors.black54)),
-                  Text("Hosting",
-                      style: GoogleFonts.poppins(color: Colors.black54))
-                ],
-              )),
-          const SizedBox(height: 5),
-          Expanded(
-              child: Container(
-            padding:
-                const EdgeInsets.only(left: 15, right: 15, bottom: 10, top: 0),
-            child: ListView.builder(
-                itemCount: _eventsList.eventsList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                      leading: Icon(Icons.list),
-                      trailing: Text(
-                        "GFG",
-                        style: TextStyle(color: Colors.green, fontSize: 15),
-                      ),
-                      title: Text("List item $index"));
-                }),
-            // child: ListView(children: [
-            //   Text("Upcoming Events",
-            //       style: GoogleFonts.poppins(
-            //           color: Colors.grey[800],
-            //           fontSize: 12,
-            //           fontWeight: FontWeight.w600)),
-            //   const SizedBox(height: 10),
-            // ]
-          )),
-        ],
-      ),
-    );
+      Container(
+        height: 50,
+        width: width,
+        margin: const EdgeInsets.only(top: 70),
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: width - 40,
+              decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+              padding: const EdgeInsets.only(
+                  left: 10, right: 10, top: 10, bottom: 10),
+              child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Search Events...",
+                      contentPadding: const EdgeInsets.only(top: 30, left: 20),
+                      hintStyle: GoogleFonts.poppins(
+                          color: Colors.grey[400],
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),
+                      border: InputBorder.none,
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey[500],
+                      ))),
+            )
+          ],
+        ),
+      )
+    ]);
   }
 }
--- lib/EventsPage/events_page.dart
+++ lib/EventsPage/events_page.dart
@@ -1,6 +1,8 @@
-import 'package:campus2/theme.dart';
+// import 'package:campsu/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../theme.dart';
 
 class EventsPage extends StatefulWidget {
   const EventsPage(
